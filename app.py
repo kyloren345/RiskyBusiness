55d21473eae6523ed1316ffd8b403bccaff096e5
Raidz <romulusrl29@gmail.com>
2024-05-15 21:35:14 +1000

    update 2

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-
+@app.route('/bibliography')
+def bibliography():
+    return render_template("bibliography.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
